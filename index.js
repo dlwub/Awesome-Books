54708ba910ef2f2122a508d0b7865a2b6ca08512
kusi <lawcubegsm@gmail.com>
2022-08-09 13:32:31 +0000

    Fix html linter error

--- index.js
+++ index.js
@@ -5,86 +5,94 @@ const bookTitle = document.getElementById('title-input');
 const bookAuthor = document.getElementById('author-input');
 const booksList = document.getElementById('books-list');
 
-function inArray(title){
-  for(let i=0; i < booksArray.length; i+=1 ){
-    if(booksArray[i]['title']  === title){
+function inArray(title) {
+  for (let i = 0; i < booksArray.length; i += 1) {
+    if (booksArray[i].title === title) {
       // return and the index it was found
       // console.log("found title")
-      return [true, 1]
+      return [true, 1];
     }
   }
-  return [false, -1]
+  return [false, -1];
 }
 
-//Add new book
-function addBook(title, author) {
+// Clear the
+function clearList() {
+  booksList.innerHTML = '';
+}
 
-  if(title && author) {
-    let testResult = inArray(title)
-    if ( testResult[0]){
+
+function updateBooks(book) {
+  booksList.innerHTML += `
+  <div> 
+    <p>${book.title}</p>
+    <p>${book.author}</p>    
+    <button id="${book.title}" type='button' onclick= "${a}(this.id)" >Remove</button>
+  </div>
+  <hr>
+  `;
+}
+
+// Remove book
+function removeBook(title) {
+  booksArray = booksArray.filter((book) => book.title !== title);
+  clearList();
+  // Update localstorage
+  localStorage.setItem('booksData', JSON.stringify(booksArray));
+  booksArray.forEach(updateBooks);
+  // location.reload()
+}
+const a = removeBook.name;
+// Show list of books in collection
+function displayBooks(book) {
+  booksList.innerHTML += `
+  <div> 
+    <p>${book.title}</p>
+    <p>${book.author}</p>    
+    <button id="${book.title}" type='button' onclick= "${a}(this.id)" >Remove</button>
+  </div>
+  <hr>
+  `;
+}
+
+// Add new book
+function addBook(title, author) {
+  if (title && author) {
+    const testResult = inArray(title);
+    if (testResult[0]) {
       // title in we can update author
-      booksArray[testResult[1]]["author"] = author
-      
-    }else{
-      let book = {
-        "title": title,
-        "author": author,
+      booksArray[testResult[1]].author = author;
+    } else {
+      const book = {
+        title,
+        author,
       };
       // not in
       booksArray.push(book);
       clearList();
       booksArray.forEach(displayBooks);
-      
     }
   }
-
-}
-
-//Remove book
-function removeBook(title){
-  booksArray = booksArray.filter((book) => {
-    return book.title !== title;
-  })  
-  clearList();
-  //Update localstorage
-  localStorage.setItem('booksData', JSON.stringify(booksArray));
-  booksArray.forEach(displayBooks);
-}
-
-//Clear the 
-function clearList(){
-  booksList.innerHTML = ""
-}
-
-//Show list of books in collection
-function displayBooks(book){
-  booksList.innerHTML += `
-  <div> 
-    <p>${book.title}</p>
-    <p>${book.author}</p>    
-    <button id="${book.title}" type='button' onclick={removeBook(this.id)}>Remove</button>
-  </div>
-  <hr>
-  `
 }
 
 bookForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  let title = bookTitle.value;
-  let author = bookAuthor.value;
-  if(title!=="" && author!==""){    
-    
-    addBook(title, author); 
-    //Add book to local storage
+  const title = bookTitle.value;
+  const author = bookAuthor.value;
+  if (title !== '' && author !== '') {
+    addBook(title, author);
+    // Add book to local storage
     localStorage.setItem('booksData', JSON.stringify(booksArray));
-  }   
+  }
 });
 
-//Retrieve data from local storage and populate page
-window.addEventListener('load', ()=> {
-  let booksData = JSON.parse(localStorage.getItem('booksData'));  
-  if(booksData) {
-    booksArray = booksData;    
-    booksArray.forEach(displayBooks); 
-  }  
-}); 
+// Retrieve data from local storage and populate page
+window.addEventListener('load', () => {
+  const booksData = JSON.parse(localStorage.getItem('booksData'));
+  if (booksData) {
+    booksArray = booksData;
+    booksArray.forEach(displayBooks);
+  }
+});
+
+// document.getElementByIdonclick=
