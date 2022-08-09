464ca23f3f809847d7445baa4736cc9325a63425
dlwub <dlwubi@gmail.com>
2022-08-09 13:59:49 +0300

    Update index.js file

--- index.js
+++ index.js
@@ -4,18 +4,61 @@ const bookTitle = document.getElementById('title-input');
 const bookAuthor = document.getElementById('author-input');
 const booksList = document.getElementById('books-list');
 
+//Add new book
+function addBook(title, author) {
+  let book = {
+    "title": title,
+    "author": author,
+  };
+  console.log(booksArray);
+  booksArray.push(book);
+  clearList();
+  booksArray.forEach(displayBooks);
+}
+//Remove book
+function removeBook(title){
+  booksArray = booksArray.filter((book) => {
+    return book.title !== title;
+  })  
+  clearList();
+  //Update localstorage
+  localStorage.setItem('booksData', JSON.stringify(booksArray));
+  booksArray.forEach(displayBooks);
+}
+//Clear the 
+function clearList(){
+  booksList.innerHTML = ""
+}
+
+//Show list of books in collection
+function displayBooks(book){
+  booksList.innerHTML += `
+  <div> 
+    <p>${book.title}</p>
+    <p>${book.author}</p>    
+    <button id="${book.title}" type='button' onclick={removeBook(this.id)}>Remove</button>
+  </div>
+  <hr>
+  `
+}
+
 bookForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  let title = bookTitle.getValue();
-  let author = bookAuthor.getValue();
-  let book = {
-    "Title": title,
-    "Author": author,
-  };
-  booksArray.push(book);
+  let title = bookTitle.value;
+  let author = bookAuthor.value;
+  if(title!=="" && author!==""){    
+    addBook(title, author); 
+    //Add book to local storage
+    localStorage.setItem('booksData', JSON.stringify(booksArray));
+  }   
 });
 
-for(let book in booksArray){
-  booksList.innerHTML += book.title + <br />;
-}
+//Retrieve data from local storage and populate page
+window.addEventListener('load', ()=> {
+  let booksData = JSON.parse(localStorage.getItem('booksData'));  
+  if(booksData) {
+    booksArray = booksData;    
+    booksArray.forEach(displayBooks); 
+  }  
+})
 
